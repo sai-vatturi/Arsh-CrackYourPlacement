afd3919d9dbe712793d55fe3eae6022b9aa7a021
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2024-07-22 15:40:39 +0530

    Time: 202 ms (5.05%) | Memory: 46.8 MB (9.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // two pointer method
+        // j will be updated as every non zero element
+        // i will be looping through the array
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, j);
+                j++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
