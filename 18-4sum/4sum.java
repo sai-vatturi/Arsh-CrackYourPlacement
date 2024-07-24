22872b19ac3b4718dce01070e59033efb3fb30ab
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2024-07-24 12:20:52 +0530

    Time: 19 ms (46.14%) | Memory: 44.1 MB (43.85%) - LeetSync

---
+++ 18-4sum/4sum.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        // REFER TO 3 SUM
+        // - Exactly like 3 sum problem but with extra loop and modified condition (sum of 4 elements should be target instead of 0)
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; 
+            } 
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int k = j + 1;
+                int l = nums.length - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if (sum == target) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        list.add(nums[l]);
+                        res.add(list);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    }
+                    else if (sum < target) {
+                        k++;
+                    }
+                    else {
+                        l--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
