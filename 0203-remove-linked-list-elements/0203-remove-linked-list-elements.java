1299550a3df8dec65c94640503a5b14f233971dd
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2024-08-01 11:04:33 +0530

    Update 0203-remove-linked-list-elements.java

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -34,4 +34,17 @@ class Solution {
 
         return dummyHead.next;
     }
+    // Effecive Recursion Solution
+    public ListNode removeElements(ListNode head, int val) {
+      
+       if(head == null){
+         return head;
+       }
+
+        
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;    
+        
+
+    }
 }
