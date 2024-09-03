5acbeb289c5b00651067c9b82243a832b904eb57
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2024-09-03 22:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (55.52%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+        1. For every node, we have to check if the left node and right node are equal
+        2. Similarly for every sub node, we have to check whether the right node and left node are equal
+        3. if not equal return false
+    */
+    public boolean isSymmetric(TreeNode root) {
+       if (root == null) {
+        return true;
+       }
+       return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
