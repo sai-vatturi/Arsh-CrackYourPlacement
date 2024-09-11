fb61ca5b2515ee6414d1128c47fefc7d95b54377
Sainadh V <115538210+sai-vatturi@users.noreply.github.com>
2024-09-11 20:36:00 +0530

    Update lowest-common-ancestor-of-a-binary-search-tree.java

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,6 +9,23 @@
  */
 
 class Solution {
+    /*
+        Brute Force Approach:
+        ====================
+        1. DFS once for finding the node p, and save the path in an array
+        2. DFS second time for finding the node q and save the path in an array
+        3. Compare both the arrays and check the last visited path by both and return it
+
+        Optimal Appraoach using Recursion:
+        =================================
+        1. Check if the root node is one of p and q. if yes, return the root node.
+        2. Now check for the left and right tree. Now look at the below conditions
+            -- if Root found, return that root to its parent
+            -- if Root Not found, check for the right part
+            -- if root is not found in both left and right, return the node itself
+            -- if one of the root is found, return that root to its parent
+        3. Do a dry run for better understanding
+    */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null) {
             return null;
